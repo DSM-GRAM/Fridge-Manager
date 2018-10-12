7a5d56491fd22ed7bbeb3525ab8e623c74a8433d
Woopa01 <whdnwls49@naver.com>
2018-10-13 04:42:23 +0900

    add MyFridgeList

--- Fridge-Manager/app/src/main/java/com/fridge_manager/ui/MyFridge/MyFridgeListActivity.kt
+++ Fridge-Manager/app/src/main/java/com/fridge_manager/ui/MyFridge/MyFridgeListActivity.kt
@@ -2,11 +2,33 @@ package com.fridge_manager.ui.MyFridge
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.fridge_manager.Adapter.MyFridgeListAdapter
+import com.fridge_manager.Model.MyFridgeItem
 import com.fridge_manager.R
+import kotlinx.android.synthetic.main.activity_myfridgelist.*
 
 class MyFridgeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_myfridgelist)
+
+        val sampledata = arrayListOf<MyFridgeItem>().apply {
+            for(i in 1..5)
+                add(MyFridgeItem("사무실 냉장고","2018/00/00",i.toString()))
+        }
+
+        val listadapter = MyFridgeListAdapter(sampledata)
+
+        with(myfridge_rv){
+            adapter = listadapter
+            layoutManager = object : LinearLayoutManager(this@MyFridgeListActivity){
+                override fun canScrollVertically(): Boolean {
+                    return false
+                }
+            }
+        }
+
+
     }
 }
--- Fridge-Manager/app/src/main/java/com/fridge_manager/ui/Start/StartActivity.kt
+++ Fridge-Manager/app/src/main/java/com/fridge_manager/ui/Start/StartActivity.kt
@@ -1,9 +1,12 @@
 package com.fridge_manager.ui.Start
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import com.fridge_manager.Adapter.MyFridgeListAdapter
 import com.fridge_manager.R
+import com.fridge_manager.ui.MyFridge.MyFridgeListActivity
 import kotlinx.android.synthetic.main.activity_start.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.toast
@@ -13,7 +16,10 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        start_myfridge_tv.onClick { toast("냉장고 리스트") }
+        start_myfridge_tv.onClick {
+            val intent = Intent(this@StartActivity,MyFridgeListActivity::class.java)
+            startActivity(intent)
+        }
         start_consume_pattern.onClick { toast("소비패턴 분석") }
         start_nutrition.onClick { toast("영양 분석") }
     }
