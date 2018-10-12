02bbd79924eb5a60b401ba3d7eb6b4f683ee095f
Woopa01 <whdnwls49@naver.com>
2018-10-12 17:22:16 +0900

    add MyFridgeListActivity

---
+++ Fridge-Manager/app/src/main/java/com/fridge_manager/ui/MyFridge/MyFridgeListActivity.kt
@@ -0,0 +1,12 @@
+package com.fridge_manager.ui.MyFridge
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fridge_manager.R
+
+class MyFridgeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myfridgelist)
+    }
+}
