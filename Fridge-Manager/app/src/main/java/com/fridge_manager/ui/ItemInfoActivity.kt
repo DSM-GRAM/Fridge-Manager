d79c043dff07bed3544b944c336bfebbd5ba9651
Sungyong Son <space010127@gmail.com>
2018-10-13 20:46:46 +0900

    [Update] Item Info Activity

---
+++ Fridge-Manager/app/src/main/java/com/fridge_manager/ui/ItemInfoActivity.kt
@@ -0,0 +1,13 @@
+package com.fridge_manager.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.fridge_manager.R
+
+class ItemInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_info)
+    }
+}
